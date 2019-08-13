e90c882f696aef939b77347dd83a828f9ab88798
Anastasis Dasyras <anastasisdasy@gmail.com>
2019-08-13 17:34:49 +0300

    Add getCost method on ServiceResponse class and some non functional code for encoding on controller

--- src/main/java/spring/json/api/ServiceController.java
+++ src/main/java/spring/json/api/ServiceController.java
@@ -5,6 +5,10 @@ import org.springframework.http.HttpEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -15,20 +19,26 @@ public class ServiceController {
 
 
 	@PostMapping("/serve")
-	public String serve(HttpEntity<String> httpEntity) {
+	public String serve(HttpEntity<String> httpEntity) throws UnsupportedEncodingException {
 
 
 		String reqObject = httpEntity.getBody();
 		System.out.println("request json object = "+reqObject);
 
 		//Get the service
+		JSONObject obj = new JSONObject(reqObject);
+		String service = obj.getJSONObject("queryResult").getJSONObject("parameters").getString("Services");
+		System.out.println(service);
+		String response = "{\"fulfillmentText\": \"I will send you information about "+service+"\""+"}";
+		//try to encode string BUT do not change anything on Dialogflow
+		String encodedString = new String(response.getBytes(),"utf-8"); 
+		System.out.println(encodedString);
+		
+		//Get service cost
+		ServiceResponse sr = new ServiceResponse(service);
+		String cost = sr.getCost();
+		System.out.println("cost: "+cost);
 
-			JSONObject obj = new JSONObject(reqObject);
-			String service = obj.getJSONObject("queryResult").getJSONObject("parameters").getString("Services");
-			System.out.println(service);
-			String response = "{\"fulfillmentText\": \"I will send you information about "+service+"\""+"}";
-
-
-		return response;
+		return encodedString;
 	}
 }
