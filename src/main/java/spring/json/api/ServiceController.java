f4c1c71eadc36daf4676d239bc04c9d1bc39de0d
AnastasiosDasyras <dasyras@tessera.gr>
2019-08-19 13:08:30 +0300

    Fix language problem, display greek letters normaly

--- src/main/java/spring/json/api/ServiceController.java
+++ src/main/java/spring/json/api/ServiceController.java
@@ -6,8 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.json.JSONObject;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,10 +23,10 @@ public class ServiceController {
 
 
 	@PostMapping("/serve")
-	public String serve(HttpEntity<String> httpEntity) throws UnsupportedEncodingException {
+	public String serve(HttpServletRequest request) throws IOException {
 
 
-		String reqObject = httpEntity.getBody();
+		String reqObject = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
 		System.out.println("request json object = "+reqObject);
 
 		//Get the service
@@ -30,15 +34,12 @@ public class ServiceController {
 		String service = obj.getJSONObject("queryResult").getJSONObject("parameters").getString("Services");
 		System.out.println(service);
 		String response = "{\"fulfillmentText\": \"I will send you information about "+service+"\""+"}";
-		//try to encode string BUT do not change anything on Dialogflow
-		String encodedString = new String(response.getBytes(),"utf-8"); 
-		System.out.println(encodedString);
 		
 		//Get service cost
 		ServiceResponse sr = new ServiceResponse(service);
 		String cost = sr.getCost();
 		System.out.println("cost: "+cost);
 
-		return encodedString;
+		return response;
 	}
 }
